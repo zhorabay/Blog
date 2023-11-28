7ca202e697a0ea6ae97fd42b1445abd5d3fdf68e
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-11-28 14:29:43 +0200

    Fix Migration mistakes

--- db/migrate/20231128103156_create_comments.rb
+++ db/migrate/20231128103156_create_comments.rb
@@ -1,6 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
       t.string :text
 
       t.timestamps
