aa13c0730ed4cecfbd9ec719972e6f842070258e
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-11-29 22:03:11 +0200

    Add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,13 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  after_save :update_comments_counter
+  after_commit :update_comments_counter, on: :create
 
   private
 
   def update_comments_counter
+    return unless post.present?
+
     post.update(comments_counter: post.comments.count)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  attribute :posts_counter, :integer
+
   validates :name, presence: true
-  validates :bio, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def return_recent_posts
     posts.order(created_at: :desc).limit(3)
