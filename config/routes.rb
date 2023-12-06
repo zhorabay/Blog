4f45f7b4100f13abf876ecffd63c890a7ad6fda2
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-12-06 12:44:37 +0200

    Make app show db info

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'user#index'
-  get 'users/:id', to: 'user#show'
-  get 'users/:user_id/posts', to: 'post#index'
-  get 'users/:user_id/posts/:id', to: 'post#show'
+  root 'users#index'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
