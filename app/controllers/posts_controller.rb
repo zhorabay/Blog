36ddd04dc4f8651c811120e5b89a3d564db7b6c0
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-12-08 19:10:35 +0200

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,6 +37,6 @@ class PostsController < ApplicationController
   end
 
   def current_user
-    @current_user = User.find(id = 1)
+    @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
