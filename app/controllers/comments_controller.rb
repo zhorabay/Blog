3473a4e26658840507ef024e45bbace374fcc33b
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-12-09 00:32:30 +0200

    Make comments can be added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,15 @@
 class CommentsController < ApplicationController
   before_action :current_user
+  before_action :set_post, only: [:new, :create]
+
+  def new
+    @user = current_user
+    @comment = Comment.new
+  end
 
   def create
     @user = current_user
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params.merge(post: @post))
+    @comment = @post.comments.build(comment_params.merge(user: @user))
 
     if @comment.save
       redirect_to user_post_path(@user, @post), notice: 'Comment added successfully!'
@@ -14,6 +19,10 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:text)
